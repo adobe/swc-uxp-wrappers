907768355c5f149f8d498a65f509266b6f8170b4
Rahul B S <rbs@adobe.com>
2023-04-20 14:02:11 +0530

    example starter added is working

---
+++ projects/example-starter-webpack/README.md
@@ -0,0 +1,8 @@
+# swc-integration
+
+This project shows a use of the `spectrum-web-components` package with webpack.
+
+# Usage
+
+First install with `npm run install` or `yarn install` then start the build and run the web server with `npm run serve`. This will build the production version of the example app and serve it via `http-server`. Inspect the webpack bundle report in the `report` folder to see the resultant bundle sizes.
+To generate dist output folder run `yarn build` 
---
+++ projects/example-starter-webpack/src/index.js
@@ -0,0 +1,21 @@
+/**************************************************************************
+ *  ADOBE CONFIDENTIAL
+ *
+ *  Copyright 2022 Adobe
+ *  All Rights Reserved.
+ *
+ *  NOTICE:  All information contained herein is, and remains
+ *  the property of Adobe and its suppliers, if any. The intellectual
+ *  and technical concepts contained herein are proprietary to Adobe
+ *  and its suppliers and are protected by all applicable intellectual
+ *  property laws, including trade secret and copyright laws.
+ *  Dissemination of this information or reproduction of this material
+ *  is strictly forbidden unless prior written permission is obtained
+ *  from Adobe.
+ ***************************************************************************/
+
+// import our stylesheets
+import './styles.css';
+
+import '@spectrum-web-components/theme/sp-theme.js';
+import '@spectrum-components/banner/sp-banner.js';
