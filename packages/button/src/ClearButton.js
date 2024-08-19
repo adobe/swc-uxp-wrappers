040b7b085defaa50375311c4ab5721178f1e1a05
Sai Deepthi Majji <majji@adobe.com>
2024-08-19 09:55:26 +0530

    add keyup event listener to handle enter

--- packages/button/src/ClearButton.js
+++ packages/button/src/ClearButton.js
@@ -18,6 +18,17 @@ class UxpClearButton extends ClearButton {
     static get styles() {
         return [super.styles, styles];
     }
+
+    firstUpdated(changed) {
+        super.firstUpdated(changed);
+        this.addEventListener('keyup', this._onKeyUp);
+    }
+
+    _onKeyUp(event) {
+        if (event.code === 'Enter' || event.code === 'NumpadEnter') {
+            this.click();
+        }
+    }
 }
 
 export { UxpClearButton as ClearButton };
--- packages/button/src/CloseButton.js
+++ packages/button/src/CloseButton.js
@@ -18,6 +18,17 @@ class UxpCloseButton extends CloseButton {
     static get styles() {
         return [super.styles, styles];
     }
+
+    firstUpdated(changed) {
+        super.firstUpdated(changed);
+        this.addEventListener('keyup', this._onKeyUp);
+    }
+
+    _onKeyUp(event) {
+        if (event.code === 'Enter' || event.code === 'NumpadEnter') {
+            this.click();
+        }
+    }
 }
 
 export { UxpCloseButton as CloseButton };
