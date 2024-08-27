f4896420f7a8e6b130b955596b0ef973858804ba
Nasir Hussain <syehussa@adobe.com>
2024-08-27 11:10:47 +0530

    [UXP-20368] - Add support of SWC sp-tabs, sp-tab and sp-tab-panel (v0.37.0) in swc-uxp-wrapper for UXP

--- projects/swc-starter-webpack/src/index.js
+++ projects/swc-starter-webpack/src/index.js
@@ -64,6 +64,11 @@ import '@spectrum-web-components/table/sp-table-row.js';
 import '@spectrum-web-components/overlay/sp-overlay.js';
 import '@spectrum-web-components/overlay/overlay-trigger.js';
 
+import '@spectrum-web-components/tabs/sp-tabs.js';
+import '@spectrum-web-components/tabs/sp-tab.js';
+import '@spectrum-web-components/tabs/sp-tab-panel.js';
+import '@spectrum-web-components/tabs/sp-tabs-overflow.js';
+
 import '@spectrum-web-components/number-field/sp-number-field.js';
 import '@spectrum-web-components/search/sp-search.js';
 import '@spectrum-web-components/textfield/sp-textfield.js';
@@ -126,3 +131,4 @@ import './extended_swc_samples/my-quick-actions.js';
 import './extended_swc_samples/my-meter.js';
 import './extended_swc_samples/my-swatch.js';
 import './extended_swc_samples/my-overlay.js';
+import './extended_swc_samples/my-tabs.js';
