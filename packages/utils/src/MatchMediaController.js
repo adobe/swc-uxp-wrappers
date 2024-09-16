a546428b6e68fec24c108fbab7927cce19223ee1
Nasir Hussain <syehussa@adobe.com>
2024-09-16 10:09:40 +0530

    fixed review comments

--- packages/utils/src/MatchMediaController.js
+++ packages/utils/src/MatchMediaController.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2023 Adobe. All rights reserved.
+Copyright 2024 Adobe. All rights reserved.
 This file is licensed to you under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License. You may obtain a copy
 of the License at http://www.apache.org/licenses/LICENSE-2.0
@@ -10,7 +10,14 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 
-// Custom  MatchMediaController
+/* 
+Custom  MatchMediaController Implementation.
+MatchMediaController is a class in reactive-controllers conponent of SWC - https://opensource.adobe.com/spectrum-web-components/tools/match-media/
+which deploys window.matchMedia(query) to accompalish various tasks, one among that is to find a mobile platform/screen and based on that rendering sp-picker or sp-tray.
+since UXP lacks support of window.matchMedia, we have written this custom implementation to overcome missing support in UXP.
+Also, UXP targets desktop application we can safely return false as mobile is not a use-case for UXP.
+*/
+
 export function MatchMediaController(context, isMobile) {
     return false;
 }
